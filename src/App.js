688167a707ca96a8768856364d854396382f6253
Graf-J <johannes.graf.1908@gmail.com>
2021-06-13 20:39:10 +0100

    Added Images

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <div className="container">
+        <h1>Grammsaufa</h1>
+      </div>
     </div>
   );
 }
